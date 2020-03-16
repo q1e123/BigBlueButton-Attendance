919c3af500d3873df987ac4c116fffa005e0824e
Robert Solca <robertnicolaesolca@gmail.com>
2020-03-16 19:12:29 +0200

    Almost finished Engine

--- main.py
+++ main.py
@@ -1,20 +1,62 @@
 import time
+import itertools
+
 
 from driver_ff import DriverFirefox
 
+from spreadsheets_driver import SpreadSheetDriver
+
 class Engine:
-    def __init__(self,dict={}):
+    def __init__(self,file_name):
         self.driver = DriverFirefox()
-        self.settings = dict
+        self.settings = self.__get_settings(file_name)
+        self.users = []
+
     def join_sesssion(self):
         self.__login()
         self.driver.goto(self.settings["link"])
         time.sleep(5)
         self.driver.click('//*[@id="join_button_input"]')
-    
+        time.sleep(10)
+        self.driver.click('/html/body/div[2]/div/div/header/button/span[1]/i')
+        time.sleep(5)
     def get_current_users(self):
-        users = []
-       
+        # get users
+        
+        for i in itertools.count(1):
+            try:
+                user = self.driver.get_value('//*[@id="app"]/main/section/div[2]/div/div/div/div[3]/div[2]/div/div/div[{}]/div/div[1]/div/div[2]/span/span'.format(str(i)),'innerHTML')
+                user = user[:-5]
+                self.users.append(user)           
+            except:
+                print('Exited from divs')
+                break
+        out = open(self.settings['output'],'w+')
+        for user in self.users:
+           out.write(user) 
+        out.close()
+
+    def put_attendance(self):
+        if self.settings['spreadsheet'] == '-':
+            return
+
+        ss_driver = SpreadSheetDriver(spreadsheet, self.settings['spreadsheet_name'])
+        ss_driver.open()
+        for user in self.users:
+            __put_user(user)            
+
+    def __get_settings(self,file_name):
+        settings = {}
+        f = open(file_name,'r')
+        lines = f.readlines()
+        for line in lines:
+            split = line.replace('\n',' ').split(' ')
+            settings[split[0]] = split[1]
+        f.close()
+        return settings
+
+    def __put_user(user):
+        return
 
     def __login(self):
         self.driver.goto(self.settings['login'])
@@ -22,18 +64,6 @@ class Engine:
         self.driver.send_keys('//*[@id="password"]',self.settings['pass'])
         self.driver.click('//*[@id="loginbtn"]')
  
-
-def get_settings(file_name):
-    settings = {}
-    f = open(file_name,'r')
-    lines = f.readlines()
-    for line in lines:
-        split = line.replace('\n',' ').split(' ')
-        settings[split[0]] = split[1]
-    return settings
-
-d = get_settings('config')
-
-e = Engine(d)
+e = Engine('config')
 e.join_sesssion()
-
+e.get_current_users()
---
+++ spreadsheets_driver.py
@@ -0,0 +1,18 @@
+
+
+class SpreadSheetDriver():
+    def __init__(self,spread_sheet_type, file_name):
+        self.file_name = file_name
+        return spread_sheet_type
+    
+    def open(self,):
+        return
+
+    def close():
+        return
+
+    def read():
+        return
+
+    def write():
+        return
